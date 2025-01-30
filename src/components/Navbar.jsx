1b32619d81426f171f960c384e5fc6ee95eff68a
Richard Orekoya <orekoyadare12@gmail.com>
2025-01-30 23:14:00 +0100

    Add saved jobs to render in dashboard page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,20 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <nav className="bg-indigo-500 p-4">
       <div className="container mx-auto flex justify-between items-center">
-        <h1 className="text-white text-2xl font-bold">AI Job Navigator</h1>
+        <Link to="/" className="text-white text-2xl font-bold cursor-pointer">
+          AI Job Navigator
+        </Link>
         <ul className="flex space-x-4 text-white">
-          <li className="hover:underline cursor-pointer">Home</li>
-          <li className="hover:underline cursor-pointer">Dashboard</li>
+          <Link to="/" className="hover:underline cursor-pointer">
+            Home
+          </Link>
+          <Link to="/dashboard" className="hover:underline cursor-pointer">
+            Dashboard
+          </Link>
           <li className="hover:underline cursor-pointer">Profile</li>
         </ul>
       </div>
